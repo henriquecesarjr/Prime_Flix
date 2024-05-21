e8a3ee51c99c88776e7e9a40c54c4e1ea5b912a8
Henrique Marques <132225341+henriquecesarjr@users.noreply.github.com>
2024-05-21 12:04:51 -0300

    Page Movie Details

--- src/pages/Filmes/index.js
+++ src/pages/Filmes/index.js
@@ -1,9 +1,65 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import './filme-info.css';
 
+import api from '../../services/api';
 
 function Filme() {
+  const { id } = useParams();
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api.get(`/movie/${id}`, {
+        params:{
+          api_key: "28a20961c584e2ce2ad03be12c00c1b2",
+          language: "pt-BR"
+        }
+      })
+      .then((response) => {
+        setFilme(response.data);
+        setLoading(false);
+      })
+      .catch(() => {
+        console.log("FILME NAO ENCONTRADO");
+      })
+    }
+
+    loadFilme();
+
+    return () => {
+      console.log("Componente foi desmontado")
+    }
+
+  }, []);
+
+  if(loading) {
+    return(
+      <div className="filme-info">
+        <h1>Carregando detalhes...</h1>
+      </div>
+    );
+  }
+
   return(
-    <div>
-      <h1>Bem vindo a pagina detalhes do filme</h1>
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+      <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+      <div className="area-buttons">
+        <button>Salvar</button>
+        <button>
+          <a href="#">
+            Trailer
+          </a>
+        </button>
+      </div>
+
     </div>
   );
 }
